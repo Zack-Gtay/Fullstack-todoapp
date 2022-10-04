234e14f410d661d6d808053c708e3dd882c696ae
zack-gtay <zakaria.gtay@final.edu.tr>
2022-10-04 19:58:35 +0300

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import Counters from "./components/Counters";
+import Movies from "./components/Movies/Movies";
 import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <div className="container pt-3">
-        <Counters />
-      </div>
-    </React.Fragment>
-  );
-}
+export default class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 0 },
+    ],
+  };
+  constructor() {
+    super();
+    console.log("App- Constructor");
+  }
+  componentDidMount() {
+    // Ajax CAll
+    console.log("App - Mounted");
+  }
 
-export default App;
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(
+      (counter) => counter.id !== counterId
+    );
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value++;
+    this.setState({ counters });
+    console.log(counters[index]);
+  };
+  handleOnDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index].value--;
+    this.setState({ counters });
+    console.log(counters[index]);
+  };
+  render() {
+    console.log("App - Rendered");
+    return (
+      <React.Fragment>
+        <Navbar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        {/* <div className="container pt-3">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleOnDecrement}
+            onDelete={this.handleDelete}
+          />
+        </div> */}
+        <Movies />
+      </React.Fragment>
+    );
+  }
+}
